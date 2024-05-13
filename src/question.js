8ee7fb7abb153cf69bb2761f78b80aec7df5ff3e
Filipa <filipamnmlopes@hotmail.com>
2024-05-13 17:11:12 +0100

    final solution for today

--- src/question.js
+++ src/question.js
@@ -5,54 +5,13 @@ class Question {
     this.answer = answer;
     this.difficulty = difficulty;
   }
-  shuffleChoices(){
-    const randomizedArr = [];
-    counter = this.choices.length // 4
-    while (counter > 0){
-        let shuffleChoice = thisChoices[(Math.floor(Math.random() * thisChoices.length))];
-        if (!randomizedArr.includes(shuffleChoice)) {
-            randomizedArr.push(shuffleChoice)
-            counter -= 1;
-        }    
-        
-        }
-        return randomizedArr
-  }
-
-
-//   shuffleChoices() {
-//     for (let i = this.choices.length-1; i > 0; i--){
-//         let k = (Math.floor(Math.random() * (i+1)));
-//         [this.choices[i], this.choices[k]] = [this.choices[k], this.choices[i]]
-        
-        
-//         //randomizedArr.push(shuffleChoice)    
-//      
-//      //return randomizedArr;
-// }
-//   }
-
-}
-
-// thisChoices = ["choice1", "choice2", "choice3", "choice4"]
-// const randomizedArr = [];
-// counter = thisChoices.length
-// while (counter > 0){
-//     let shuffleChoice = thisChoices[(Math.floor(Math.random() * thisChoices.length))];
-//     if (!randomizedArr.includes(shuffleChoice)) {
-//         randomizedArr.push(shuffleChoice)
-//         counter -= 1;
-//     }    
-    
-//     }
-// console.log(randomizedArr)
-
-
-
-/*function shuffleArray(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
+  shuffleChoices() {
+    for (let i = 0; i < this.choices.length; i++) {
+      let randomIndex = Math.floor(Math.random() * this.choices.length);
+      let randomElement = this.choices[randomIndex];
+      this.choices.splice(randomElement, 1);
+      this.choices.push(randomElement);
     }
-}*/
-
+    
+  }
+}
--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,41 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
+    constructor(questions, timeLimit, timeRemaining){
+        this.questions = questions;
+        this.timeLimit = timeLimit;
+        this.timeRemaining = timeRemaining;
+        this.correctAnswers = 0;
+        this.currentQuestionIndex = 0;
+    }
 
-    // 2. getQuestion()
+    getQuestion(){
+        return this.questions[this.currentQuestionIndex]
+    }
     
-    // 3. moveToNextQuestion()
+    moveToNextQuestion(){
+        return this.questions[this.currentQuestionIndex++]
+    }
 
-    // 4. shuffleQuestions()
+    shuffleQuestions(){
+        for (let i = 0; i < this.questions.length; i++) {
+            let randomIndex = Math.floor(Math.random() * this.questions.length);
+            let randomElement = this.questions[randomIndex];
+            this.questions.splice(randomElement, 1);
+            this.questions.push(randomElement);
+          }
+    }
 
-    // 5. checkAnswer(answer)
+    checkAnswer(answer){
+        if(this.questions[this.currentQuestionIndex].answer === answer){
+            return this.correctAnswers ++;
+        }
+    }
 
-    // 6. hasEnded()
+
+    hasEnded(){
+        if(this.currentQuestionIndex === this.questions.length){
+            return true;
+        }else{
+            return false;
+        }
+    }
 }
